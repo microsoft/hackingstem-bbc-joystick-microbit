9405286836cd2c538fb9b77170d823baf11a1cf5
Jeremy Franklin-Ross <v-jerfra@microsoft.com>
2018-10-08 11:47:59 -0700

    fixing timeout bug

--- joystick.py
+++ joystick.py
@@ -1,23 +1,47 @@
-#   Joystick Controller Code for Microsoft HackingSTEM + BBC Lesson
-#   For more information go to aka.ms/hackingstem
-#   V1.2
-#   Change Log
-#   - Reset Function Included
-#   - gameState changed to hitTime
-#   - isensor nput pairs reversed
+# ------------__ Hacking STEM – joystick.py – micro:bit __-----------
+#  For use with the How do sharks swim lesson plan from Microsoft
+#  Education Workshop at http://aka.ms/hackingSTEM
+# 
+#  Uses pins 0,1,2,3,6,7
+#   Roll Counter Clockwise = pin1
+#   Roll Clockwise = pin0
+#   Pitch Counter Clockwise = pin3
+#   Pitch Clockwise = pin2
+#   Yaw Counter Clockwise = pin7
+#   Yaw Clockwise = pin6
 #
-#   Written by Adi Azulay
-#   Copyright 2018 Microsoft
+#  Note: This project has a counter intuitive relationship with 
+#  excel. Much of the code is to handle serial data from excel
+#  which is immediately returned to it in subsequent serial writes.
+#
+#  This project uses a BBC micro:bit microcontroller, information at:
+#  https://microbit.org/
+#
+#  Comments, contributions, suggestions, bug reports, and feature
+#  requests are welcome! For source code and bug reports see:
+#  http://github.com/[TODO github path to Hacking STEM]
+#
+#  Copyright 2018, Adi Azulay w/ contributions by Jeremy Franklin-Ross
+#  Microsoft EDU Workshop - HackingSTEM
+#  MIT License terms detailed in LICENSE.txt
+# ===---------------------------------------------------------------===
 
 from microbit import *
 
-DEFAULT_RETURN_STRING = ",0,-10.5,0"
-current_return_string = DEFAULT_RETURN_STRING
-parsed_data = ""
-comma_count = 0
-last_read = running_time() 
-SENSOR_INTERVAL = 250
+# Used by excel to home shark graphic
+DEFAULT_RETURN_STRING = ",0,-10.5,0" 
+
+# Working variable 
+current_return_string = DEFAULT_RETURN_STRING  
+
+# Variables used in dealing with serial in data 
+parsed_data = "" 
 last_data = ""
+comma_count = 0
+
+# How often we poll joystick
+last_read = running_time() 
+SENSOR_INTERVAL = 100
 
 #  Directional Variables
 pitch = 0
@@ -63,51 +87,39 @@ def processSensors():
 	else:
 		yaw = 0
 
-in_data = ""
 def getData():
-	#   This function gets data from serial and builds it into a string
-	#TX,0,-3,0,0,,,,,,,
 	global current_return_string, parsed_data, last_data
 	
-
 	while uart.any() is True: # and parsed_data.count('\n') == 0: 
 		parsed_data += str(uart.read(150), "utf-8", "ignore")
 		sleep(10)
 
-	comma_count = parsed_data.count(",")
+	# make sure we have a well formed serial read.
+	comma_count = parsed_data.count(",") 
 
-	# if (comma_count > 11 or comma_count < 7):
 	if ( comma_count != 7):
-		# uart.write("{},{},{},".format(roll, pitch, yaw)) 
-		# uart.write(current_return_string)
-		# uart.write(",,,,,,,FAILED   FFFFFFFF   FAILED:" + parsed_data[:-1] + "###" + str(comma_count) + "###\n")
 		parsed_data = ""
-		return
+		return  # skip this method if not formed well
 
 
 	if len(parsed_data) > 0 and parsed_data.endswith("\n"):
 		parsed_data = parsed_data.rstrip(',\n\r')
 		parsed_data = parsed_data.rstrip()
 		
-		if last_data == parsed_data:
+		# if there's no change, skip the rest of this method
+		if last_data == parsed_data: 
+			parsed_data = ""
 			return
 
 		last_data = parsed_data
 
-		if parsed_data.endswith(",1"):
-			current_return_string = DEFAULT_RETURN_STRING + ",RESET  rrrrrrrrrrrrrrrrrr RESET" + str(comma_count) + "#"
-			#uart.write("current now DEFAULT {}".format(current_return_string)+"\n")
+		if parsed_data.endswith(",1"):  # reset to home value 
+			current_return_string = DEFAULT_RETURN_STRING 
 		elif parsed_data.endswith(",0"):
-			current_return_string = parsed_data + ",UPDATE T:" + str(running_time()) + "#" + str(comma_count) + "#"
-			#:-1 peels off the "\n"
-			#uart.write("current now parsed {}".format(current_return_string)+"\n")
-		else:
-			uart.write("{},{},{},".format(roll, pitch, yaw)) 
-			uart.write(current_return_string)
-			uart.write(",,,,,,,,ERROR    EEEEEEEE      ERROR:," + parsed_data + "###\n")
+			current_return_string = parsed_data 
+
 		parsed_data = ""
-		# sleep(500)
-		
+
 
 uart.init(9600)
 display.off()
@@ -122,7 +134,5 @@ while True:
 	uart.write("{},{},{},".format(roll, pitch, yaw)) 
 	uart.write(current_return_string)
 	uart.write("\n")
-	# sleep(250)
 
-#	print("{},{},{},{}"roll, pitch, yaw, current_return_string)
 	sleep(10)
